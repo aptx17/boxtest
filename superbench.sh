eccbe2be5b6687da8da6a281fc90e517ee7033a7
aptx17 <41042297+aptx17@users.noreply.github.com>
2023-03-16 23:24:28 +0800

    Update superbench.sh

--- superbench.sh
+++ superbench.sh
@@ -734,6 +734,31 @@ function UnlockiQiyiIntlTest() {
     fi
 }
 
+function UnlockChatGPTTest() {
+	if [[ $(curl --max-time 10 -sS https://chat.openai.com/ -I | grep "text/plain") != "" ]]
+	then
+        echo -e " ChatGPT              : ${RED}IP is BLOCKED${PLAIN}" | tee -a $log
+        return
+	fi
+    local countryCode="$(curl --max-time 10 -sS https://chat.openai.com/cdn-cgi/trace | grep "loc=" | awk -F= '{print $2}')";
+	if [ $? -eq 1 ]; then
+        echo -e " ChatGPT              : ${RED}Network connection failed${PLAIN}" | tee -a $log
+        return
+    fi
+	if [ -n "$countryCode" ]; then
+        support_countryCodes=(T1 XX AL DZ AD AO AG AR AM AU AT AZ BS BD BB BE BZ BJ BT BA BW BR BG BF CV CA CL CO KM CR HR CY DK DJ DM DO EC SV EE FJ FI FR GA GM GE DE GH GR GD GT GN GW GY HT HN HU IS IN ID IQ IE IL IT JM JP JO KZ KE KI KW KG LV LB LS LR LI LT LU MG MW MY MV ML MT MH MR MU MX MC MN ME MA MZ MM NA NR NP NL NZ NI NE NG MK NO OM PK PW PA PG PE PH PL PT QA RO RW KN LC VC WS SM ST SN RS SC SL SG SK SI SB ZA ES LK SR SE CH TH TG TO TT TN TR TV UG AE US UY VU ZM BO BN CG CZ VA FM MD PS KR TW TZ TL GB)
+		if [[ "${support_countryCodes[@]}"  =~ "${countryCode}" ]];  then
+            echo -e " ChatGPT              : ${GREEN}Yes (Region: ${countryCode})${PLAIN}" | tee -a $log
+            return
+        else
+			echo -e " ChatGPT              : ${RED}No${PLAIN}" | tee -a $log
+            return
+        fi
+    else
+        echo -e " ChatGPT              : ${RED}Failed${PLAIN}" | tee -a $log
+        return
+    fi
+}
 
 function StreamingMediaUnlockTest(){
 	echo -e " Stream Media Unlock  :" | tee -a $log
@@ -743,6 +768,7 @@ function StreamingMediaUnlockTest(){
 	UnlockBilibiliTest
 	UnlockTiktokTest
 	UnlockiQiyiIntlTest
+	UnlockChatGPTTest
 }
 
 sharetest() {
