65c96e81691ad569b5c5f0d57ad310f50cfbc5a0
aptx17 <41042297+aptx17@users.noreply.github.com>
2024-03-19 07:01:04 +0800

    Update build_action.sh

--- build_action.sh
+++ build_action.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # install dep 
 apt update
-apt install -y wget xz-utils make gcc flex bison dpkg-dev bc rsync kmod cpio libssl-dev libelf-dev git build-essential libncurses5-dev gcc-multilib g++-multilib python3 python3-pip python3-ply
+apt install -y wget xz-utils make gcc flex bison dpkg-dev bc rsync kmod cpio libssl-dev libelf-dev apt-utils lsb-release git build-essential libncurses5-dev gcc-multilib g++-multilib python3 python3-pip python3-ply
 apt build-dep -y linux
 
 # download kernel source
@@ -21,7 +21,7 @@ make oldconfig
 scripts/config --disable SECURITY_LOCKDOWN_LSM
 scripts/config --disable DEBUG_INFO
 scripts/config --disable MODULE_SIG
-#scripts/config --disable CONFIG_SLUB_DEBUG
+scripts/config --disable CONFIG_SLUB_DEBUG
 # build deb packages
 CPU_CORES=$(($(grep -c processor < /proc/cpuinfo)*2))
 make deb-pkg -j"$CPU_CORES"
