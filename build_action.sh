d8468787769e23c94ae5d401bba5c7b84ce9eb7f
aptx17 <41042297+aptx17@users.noreply.github.com>
2024-03-19 06:26:56 +0800

    Update build_action.sh

--- build_action.sh
+++ build_action.sh
@@ -5,13 +5,13 @@ apt install -y wget xz-utils make gcc flex bison dpkg-dev bc rsync kmod cpio lib
 apt build-dep -y linux
 
 # download kernel source
-wget https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.1.66.tar.xz
-tar -xf linux-6.1.66.tar.xz
-cd linux-6.1.66 || exit
+wget https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.6.22.tar.xz
+tar -xf linux-6.6.22.tar.xz
+cd linux-6.6.22 || exit
 
 # apply patches
-cp ../convert_official_linux-6.1.x_src_to_bbrplus.patch .
-patch -p1 < convert_official_linux-6.1.x_src_to_bbrplus.patch
+cp ../convert_official_linux-6.6.x_src_to_bbrplus.patch .
+patch -p1 < convert_official_linux-6.6.x_src_to_bbrplus.patch
 
 # x86-config
 cp ../config .config
