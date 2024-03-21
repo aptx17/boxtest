74f27dc000e49b4a62ea6883edb79d6d3df81459
aptx17 <41042297+aptx17@users.noreply.github.com>
2024-03-22 03:21:26 +0800

    Update build_action.sh

--- build_action.sh
+++ build_action.sh
@@ -2,13 +2,13 @@
 sudo apt build-dep -y linux
 
 # download kernel source
-wget -q https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.1.81.tar.xz
-tar -xf linux-6.1.81.tar.xz
-cd linux-6.1.81 || exit
+wget -q https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.6.22.tar.xz
+tar -xf linux-6.6.22.tar.xz
+cd linux-6.6.22 || exit
 
 # apply patches
-cp ../convert_official_linux-6.1.x_src_to_bbrplus.patch ./
-patch -p1 < convert_official_linux-6.1.x_src_to_bbrplus.patch
+cp ../convert_official_linux-6.6.x_src_to_bbrplus.patch ./
+patch -p1 < convert_official_linux-6.6.x_src_to_bbrplus.patch
 
 # x86-config
 cp -f ../.config ./
