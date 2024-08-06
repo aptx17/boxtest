9d7708fbbc3458c751827f6f90efe2179b04bbc8
aptx17 <41042297+aptx17@users.noreply.github.com>
2024-08-06 19:12:08 +0800

    Create build_action-arm.sh

---
+++ build_action-arm.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# install dep
+apt update -y
+apt install -y wget jq gpg xz-utils make gcc flex bison dpkg-dev bc u-boot-tools dpkg-dev kernel-package gcc-arm-linux-gnueabihf rsync kmod cpio libssl-dev libelf-dev apt-utils git build-essential libncurses5-dev gcc-multilib g++-multilib python3 python3-pip python3-ply debhelper
+apt build-dep -y linux
+
+# download kernel source
+wget -q https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.6.44.tar.xz
+tar -xf linux-6.6.44.tar.xz
+cd linux-6.6.44 || exit
+
+# apply patches
+cp ../convert_official_linux-6.6.x_src_to_bbrplus.patch ./
+patch -p1 < convert_official_linux-6.6.x_src_to_bbrplus.patch
+
+# x86-config
+cp -f ../.config ./
+make oldconfig
+
+# disable debug info & module signing
+scripts/config --disable SECURITY_LOCKDOWN_LSM
+scripts/config --disable DEBUG_INFO
+scripts/config --disable MODULE_SIG
+scripts/config --disable CONFIG_SLUB_DEBUG
+# build deb packages
+CPU_CORES=$(($(grep -c processor < /proc/cpuinfo)*2))
+make bindeb-pkg -j"$CPU_CORES" LOCALVERSION=-custom
